53c3f852db5d2d0c9cf54ddc7e86f60ef8c64dda
Juan Pablo Garcia Ripa <sarabadu@gmail.com>
2020-11-16 10:26:36 -0300

    just WIP thinking a lot

---
+++ jobs.js
@@ -0,0 +1,54 @@
+export const jobs = [
+    {
+        company: "SOUTHWORKS",
+        startDate: "2020-06",
+        endDate: "current",
+        titles: [
+            {
+                title: "Senior Developer",
+                startDate: "2020-06",
+                endDate: "current",
+                location: "Remote, Argentina",
+                summary: "",
+                summaryBullets: [
+                    "Frontend Development (React, Vue, Angular 2, Angular.js)",
+                    "Backend Development (C#)",
+                    "Requirement Analysis",
+                    "Technologies Research",
+                    "Troubleshooting"
+                ]
+            }
+        ]
+
+
+    },
+    {
+        company: "MOBEATS",
+        startDate: "2019-11",
+        endDate: "2020-06",
+        titles: [
+            {
+                title: "Fullstack Software Developer | Tech Lead | Teacher",
+                startDate: "2019-11",
+                endDate: "2020-06",
+                location: "Berazategui, Buenos Aires, Argentina",
+                summary: "",
+                summaryBullets: [
+                    "Teach Lead on Java and JavaScript projects (React, React-Native, Angular2)",
+                    "Mentoring of Junior developers",
+                    "Architecture and Solution consultancy",
+                    "Teaching on JavaScript workshops path"
+                ]
+            }
+        ]
+
+
+    }
+]
+
+
+
+
+
+
+
